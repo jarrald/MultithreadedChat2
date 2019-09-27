0c464cf31852fc33e357c430debef5724ed69d1e
Jarrald <novijgod@yahoo.dk>
2019-09-27 11:37:16 +0200

    Initial

---
+++ src/kea/dat18c/multithreadedchat/client/WriteThread.java
@@ -0,0 +1,54 @@
+package kea.dat18c.multithreadedchat.client;
+
+import java.io.*;
+import java.net.Socket;
+
+public class WriteThread extends Thread {
+    private PrintWriter writer;
+    private Socket socket;
+    private ChatClient client;
+
+    public WriteThread(Socket socket, ChatClient client) {
+        this.socket = socket;
+        this.client = client;
+
+        try {
+            OutputStream output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
+        } catch (IOException ex) {
+            System.out.println("Error getting output stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+    public void run() {
+        try {
+            Console console = System.console();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.print("\nEnter your name: ");
+            //String userName = null;
+            String userName = reader.readLine();
+
+            client.setUserName(userName);
+            writer.println(userName);
+
+            String text;
+
+            do {
+                //System.out.print("["+userName+"]2: ");
+                text = reader.readLine();
+                writer.println(text);
+
+            } while (!text.equals("bye"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException ex) {
+
+            System.out.println("Error writing to server: " + ex.getMessage());
+        }
+    }
+}
